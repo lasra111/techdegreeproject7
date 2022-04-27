22f2906796be0bc8d444d0106057563bf5a2da74
lasra111 <90154983+lasra111@users.noreply.github.com>
2022-04-27 16:00:28 -0600

    changes

--- app.js
+++ app.js
@@ -68,3 +68,95 @@ send.addEventListener('click', ()=> {
     alert('Message sent!');
   }
 });
+
+    let ctx = document.getElementById('lineChart').getContext('2d');
+    let myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10','11-17', '18-24', '25-31' ],
+            datasets: [{
+                label: 'Traffic',
+                data: [750, 1250,1000 , 2000, 1500,1750, 1250, 1850, 2250, 1500, 2500 ],
+                backgroundColor: 'rgba(112, 104, 201, .5)',
+
+                borderWidth: 1
+            }]
+        },
+        options: {
+          backgroundColor: 'rgba(112, 104, 201, .5)',
+          fill: true,
+          aspectRatio: 2,
+          animation: {
+          duration: 0
+          },
+          scales: {
+          y: {
+          beginAtZero: true
+          }
+          },
+          plugins: {
+          legend: {
+          display: false
+          }
+          }
+          }
+    });
+  
+    let ctxx = document.getElementById('dailyChart').getContext('2d');
+    let myChart2 = new Chart(ctxx, {
+        type: 'bar',
+        data: {
+            labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+            datasets: [{
+                label: '# of Hits',
+                data: [75, 115, 175, 125, 225, 200, 100],
+                backgroundColor: '#7561ab',
+
+                borderWidth: 1
+            }]
+        },
+        options: {
+          scales: {
+          y: {
+          beginAtZero: true
+          }
+          },
+          plugins: {
+          legend: {
+          display: false
+          }
+          }
+          }
+    });
+
+
+
+    let ctxxx = document.getElementById('pieChart').getContext('2d');
+    let myChart3 = new Chart(ctxxx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Desktop', 'Tablet', 'Phones'],
+            datasets: [{
+                label: '# of Hits',
+                data: [2000, 550, 500],
+                borderWidth: 0,
+                backgroundColor: [
+                '#7477BF',
+                '#78CF82',
+                '#51B6C8'
+                ]
+            }]
+        },
+        options: {
+          aspectRatio: 1.9,
+          plugins: {
+          legend: {
+          position: 'right',
+          labels: {
+          boxWidth: 20,
+          fontStyle: 'bold'
+          }
+          }
+          }
+          }
+          });
